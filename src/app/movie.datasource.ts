15098385e70bb61b575b805e78744f1b8a679d30
PatatesMovie <infohuseyinkadioglu@gmail.com>
2020-02-19 16:25:55 +0300

    Observable'a giriş

---
+++ src/app/movie.datasource.ts
@@ -0,0 +1,10 @@
+import {Movie} from './movie';
+
+
+export const Movies: Movie[] = [
+  {id: 1, name : 'Inception'},
+  {id: 2 , name: 'Shawshank'},
+  {id: 3, name: 'Eternal Sunshine'} ,
+  {id: 4 , name: 'Moonlight'},
+  {id: 5, name: 'Once upon a Time'}
+];
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,10 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Movie} from '../movie';
+import {MovieService} from '../movie.service';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'movies',
-  templateUrl: 'movies.component.html'
+  templateUrl: 'movies.component.html',
+ // templateUrl: 'movies.component.html',
+  styleUrls: ['movies.component.css']
 })
-export class MoviesComponent {
+export class MoviesComponent implements  OnInit {
+  title = 'Movie List';
+  movies: Movie[];
+  selectedMovie: Movie;
 
+  constructor(private movieService: MovieService) {}
+  ngOnInit(): void {
+   this.getMovies();
+  }
+
+
+  onSelect(movie: Movie): void {
+    this.selectedMovie = movie;
+  }
+  getMovies(): void {
+      this.movieService.getMovies()
+           .subscribe(movies => {
+              this.movies = movies;
+    });
+
+  }
 }
